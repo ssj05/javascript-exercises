2456bfd63a74d885de497d2b20407caa518523df
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-02 20:50:25 +0530

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, length) {
+  if (length < 0) {
+    return "ERROR";
+  }
+  let repeatWord = "";
+  for (let i = 0; i < length; i++) {
+    repeatWord += word;
+  }
+  return repeatWord;
 };
 
 // Do not edit below this line
