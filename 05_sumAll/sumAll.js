a25ab9ad50196bc5194c800c08ed5e62938ff640
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-03 15:23:28 +0530

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
+  let sum = 0;
 
+  let firstNum = 0;
+  let lastNum = 0;
+  if (num1 > num2) {
+    firstNum = num2;
+    lastNum = num1;
+  } else {
+    firstNum = num1;
+    lastNum = num2;
+  }
+  for (let i = firstNum; i <= lastNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
