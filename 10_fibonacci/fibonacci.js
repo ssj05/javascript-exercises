eb97b1e1b02e5824c4637082eb857d0b0d5e0eaa
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-25 22:00:25 +0530

    complete 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0 || n === "0") {
+    return 0;
+  }
+  let a = 1;
+  let b = 1;
 
+  for (let i = 0; i < parseInt(n); i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+    if (i === n - 2 || parseInt(n) === 1) {
+      return a;
+    }
+  }
 };
 
 // Do not edit below this line
