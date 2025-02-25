166591242145469e31fbdf49f4baf1626205d0a1
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-25 16:07:46 +0530

    complete 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+  const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumeric.includes(char))
+    .join("");
+
+  const reversedStr = cleanStr.split("").reverse().join("");
+
+  return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
