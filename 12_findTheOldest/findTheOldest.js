d11ef5b6617b2d12e620282e0c331abb7a380a22
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-25 22:35:52 +0530

    complete 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let obj = {};
+  let age = 0;
+  people.forEach((person) => {
+    if (typeof person.yearOfDeath === "undefined") {
+      if (new Date().getFullYear() - person.yearOfBirth > age) {
+        age = new Date().getFullYear() - person.yearOfBirth;
+        obj = person;
+      }
+    } else {
+      if (person.yearOfDeath - person.yearOfBirth > age) {
+        age = person.yearOfDeath - person.yearOfBirth;
+        obj = person;
+      }
+    }
+  });
+  return obj;
 };
 
 // Do not edit below this line
